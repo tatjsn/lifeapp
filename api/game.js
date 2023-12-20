df90c0a2ce7eb2bc2e23fc5e5092224869f56dae
Taz Jackson <tatjsn@users.noreply.github.com>
2023-12-20 16:33:12 +0000

    Reject form resubmit

--- api/game.js
+++ api/game.js
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import cookie from 'cookie';
 import view from './clients/view.js';
 import questions from './questions.json';
@@ -18,29 +19,37 @@ export default async function handler(req, res) {
   };
 
   let state = defaultState;
+  let crumb = crypto.randomUUID();
   let incorrectQuestion;
   let examSummary;
 
-  if (req.method == 'POST' && !req.headers.cookie) {
-    // unexpected flow, probably due to tab restoration and samesite=strict
-    // reload immediately and do not touch cookie
-    res.redirect(303, '/');
-    return;
-  }
-
   if (req.headers.cookie) {
     // existence of cookie doesn't meant the app is initialised
-    const { progress, score, incorrect } = cookie.parse(req.headers.cookie);
+    const {
+      progress,
+      score,
+      incorrect,
+      crumb: prevCrumb,
+    } = cookie.parse(req.headers.cookie);
     if (progress !== undefined) {
       state = {
         progress: +progress,
         score: +score,
         incorrect: incorrect ? incorrect.split(',').map((x) => +x) : [],
+        crumb: prevCrumb || '',
       };
     }
   }
 
   if (req.method == 'POST') {
+    if (
+      !!state.crumb && // backward compatibility
+      req.body.crumb !== state.crumb
+    ) {
+      // Reject form re-submit, due to reload or tab restore
+      res.redirect(303, '/');
+      return;
+    }
     const currentQ = questions[state.progress];
     if (
       currentQ.corrects
@@ -66,6 +75,7 @@ export default async function handler(req, res) {
     cookie.serialize('progress', state.progress, COOKIE_OPTIONS),
     cookie.serialize('score', state.score, COOKIE_OPTIONS),
     cookie.serialize('incorrect', state.incorrect, COOKIE_OPTIONS),
+    cookie.serialize('crumb', crumb, COOKIE_OPTIONS),
   ]);
 
   res.setHeader('Content-Type', 'text/html');
@@ -75,6 +85,7 @@ export default async function handler(req, res) {
       question: questions[state.progress],
       incorrectQuestion,
       examSummary,
+      crumb,
     }),
   );
 }
